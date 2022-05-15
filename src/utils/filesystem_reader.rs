c4f96432a20480e33428c0bc77e0f434754b4018
Damon <damonhayhurst@gmail.com>
2022-05-15 18:07:21 +0100

    change project directory values

--- src/utils/filesystem_reader.rs
+++ src/utils/filesystem_reader.rs
@@ -3,15 +3,19 @@ use std::fs::create_dir_all;
 use std::path::{PathBuf, Path};
 use std::error::Error;
 
+const QUALIFIER: &str = "";
+const ORGANIZATION: &str = "SongRec";
+const APPLICATION: &str = "SongRec";
+
 pub fn obtain_csv_path() -> Result<String, Box<dyn Error>> {
-    let project_dir = ProjectDirs::from("com", "Github", "SongRec").ok_or("No valid path")?;
+    let project_dir = ProjectDirs::from(QUALIFIER, ORGANIZATION, APPLICATION).ok_or("No valid path")?;
     let mut csv_path: PathBuf = obtain_data_dir(project_dir)?;
     csv_path.push("song_history.csv");
     Ok(csv_path.to_str().unwrap().to_string())
 }
 
 pub fn obtain_preferences_file_path() -> Result<String, Box<dyn Error>> {
-    let project_dir = ProjectDirs::from("com", "Github", "SongRec").ok_or("No valid path")?;
+    let project_dir = ProjectDirs::from(QUALIFIER, ORGANIZATION, APPLICATION).ok_or("No valid path")?;
     let mut preferences_file_path: PathBuf = obtain_preferences_dir(project_dir)?;
     preferences_file_path.push("preferences.toml");
     Ok(preferences_file_path.to_str().unwrap().to_string())
