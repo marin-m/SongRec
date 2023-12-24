f0bf43fa35b338acecac7bb0b3faae807a0f683e
Marin <marin-m@users.noreply.github.com>
2023-12-24 09:33:52 +0100

    Add the missing "libpulse-dev" dependency in the Debian package specification

--- packaging/ppa/build_binary_package.sh
+++ packaging/ppa/build_binary_package.sh
@@ -17,9 +17,9 @@ trap cleanup_dirs INT TERM
 
 rm -rf ../../target/ ../../vendor/ ../../.flatpak-builder ../flatpak/.flatpak-builder ../../repo
 
-cp -ra ../../ "${temp_dir}/songrec-0.4.0"
+cp -ra ../../ "${temp_dir}/songrec-0.4.0+1"
 
-cd "${temp_dir}/songrec-0.4.0"
+cd "${temp_dir}/songrec-0.4.0+1"
 
 mkdir -p .cargo
 cargo vendor --locked vendor | sed 's/^directory = ".*"/directory = "vendor"/g' > .cargo/config
