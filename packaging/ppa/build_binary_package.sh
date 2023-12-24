0d13825d5eb99a6fc70e8dde5978f76dc92acc53
Marin <marin-m@users.noreply.github.com>
2023-12-24 18:56:36 +0100

    Fix a typo in the Debian PPA control file

--- packaging/ppa/build_binary_package.sh
+++ packaging/ppa/build_binary_package.sh
@@ -17,9 +17,9 @@ trap cleanup_dirs INT TERM
 
 rm -rf ../../target/ ../../vendor/ ../../.flatpak-builder ../flatpak/.flatpak-builder ../../repo
 
-cp -ra ../../ "${temp_dir}/songrec-0.4.0+1"
+cp -ra ../../ "${temp_dir}/songrec-0.4.0+2"
 
-cd "${temp_dir}/songrec-0.4.0+1"
+cd "${temp_dir}/songrec-0.4.0+2"
 
 mkdir -p .cargo
 cargo vendor --locked vendor | sed 's/^directory = ".*"/directory = "vendor"/g' > .cargo/config
