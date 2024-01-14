ed4ed7b6f51c04d31c79ac6ee58b6da59bfddc80
Marin <marin-m@users.noreply.github.com>
2024-01-14 14:42:56 +0100

    Prepare releasing version 0.4.1

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,7 @@ use clap::{App, Arg};
 macro_rules! base_app {
     () => {
     App::new("SongRec")
-        .version("0.4.0")
+        .version("0.4.1")
         .about(gettext("An open-source Shazam client for Linux, written in Rust.").as_str())
         .subcommand(
             App::new("listen")
