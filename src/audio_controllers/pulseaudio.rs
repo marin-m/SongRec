bd789e01adca6a25e243a54586279a8645f61a91
Marin <marin-m@users.noreply.github.com>
2026-03-08 06:51:25 +0100

    Log PulseAudio-related errors

--- src/audio_controllers/pulseaudio.rs
+++ src/audio_controllers/pulseaudio.rs
@@ -6,7 +6,7 @@ use pulsectl::controllers::{AppControl, DeviceControl, SourceController};
 use crate::audio_controllers::audio_backend::AudioBackend;
 use crate::core::thread_messages::DeviceListItem;
 
-use log::debug;
+use log::{debug, error};
 
 pub struct PulseBackend {
     handler: SourceController,
@@ -14,11 +14,18 @@ pub struct PulseBackend {
 
 impl PulseBackend {
     pub fn try_init() -> Option<Self> {
-        if let Ok(mut handler) = SourceController::create() {
-            if let Ok(_) = handler.get_server_info() {
-                if let Ok(_) = handler.list_devices() {
+        match SourceController::create() {
+            Ok(mut handler) => {
+                if let Err(error) = handler.get_server_info() {
+                    error!("Could not get PulseAudio server info: {:?}", error);
+                } else if let Err(error) = handler.list_devices() {
+                    error!("Could not list PulseAudio devices: {:?}", error);
+                } else {
                     return Some(Self { handler });
                 }
+            }
+            Err(error) => {
+                error!("Could not initialize PulseAudio backend: {:?}", error);
             }
         }
         None
@@ -58,36 +65,44 @@ impl AudioBackend for PulseBackend {
         let mut device_names: Vec<DeviceListItem> = vec![];
         let mut monitor_device_names: Vec<DeviceListItem> = vec![];
 
-        if let Ok(info) = self.handler.get_server_info() {
-            if let Ok(devices) = self.handler.list_devices() {
-                for dev in devices {
-                    if let Some(desc) = &dev.description {
-                        if let Some(name) = &dev.name {
-                            if &dev.name == &info.default_source_name {
-                                device_names.insert(
-                                    0,
-                                    DeviceListItem {
+        match self.handler.get_server_info() {
+            Ok(info) => match self.handler.list_devices() {
+                Ok(devices) => {
+                    for dev in devices {
+                        if let Some(desc) = &dev.description {
+                            if let Some(name) = &dev.name {
+                                if &dev.name == &info.default_source_name {
+                                    device_names.insert(
+                                        0,
+                                        DeviceListItem {
+                                            inner_name: name.to_string(),
+                                            display_name: desc.to_string(),
+                                            is_monitor: dev.monitor != None,
+                                        },
+                                    );
+                                } else if dev.monitor != None {
+                                    monitor_device_names.push(DeviceListItem {
                                         inner_name: name.to_string(),
                                         display_name: desc.to_string(),
-                                        is_monitor: dev.monitor != None,
-                                    },
-                                );
-                            } else if dev.monitor != None {
-                                monitor_device_names.push(DeviceListItem {
-                                    inner_name: name.to_string(),
-                                    display_name: desc.to_string(),
-                                    is_monitor: true,
-                                });
-                            } else {
-                                device_names.push(DeviceListItem {
-                                    inner_name: name.to_string(),
-                                    display_name: desc.to_string(),
-                                    is_monitor: false,
-                                });
+                                        is_monitor: true,
+                                    });
+                                } else {
+                                    device_names.push(DeviceListItem {
+                                        inner_name: name.to_string(),
+                                        display_name: desc.to_string(),
+                                        is_monitor: false,
+                                    });
+                                }
                             }
                         }
                     }
                 }
+                Err(error) => {
+                    error!("Could not list PulseAudio devices: {:?}", error);
+                }
+            },
+            Err(error) => {
+                error!("Could not get PulseAudio server info: {:?}", error);
             }
         }
 
@@ -96,20 +111,25 @@ impl AudioBackend for PulseBackend {
     }
 
     fn set_device(&mut self, host: &Host, inner_name: &str) -> Device {
-        if let Ok(devices) = self.handler.list_devices() {
-            if let Some(app_idx) = self.get_app_idx() {
-                for dev in devices {
-                    debug!(
-                        "Comparing libpulse device names: {:?} / {:?}",
-                        dev.name, inner_name
-                    );
-                    if Some(inner_name) == dev.name.as_deref() {
-                        debug!("Selected libpulse device found: {:?}", dev);
+        match self.handler.list_devices() {
+            Ok(devices) => {
+                if let Some(app_idx) = self.get_app_idx() {
+                    for dev in devices {
+                        debug!(
+                            "Comparing libpulse device names: {:?} / {:?}",
+                            dev.name, inner_name
+                        );
+                        if Some(inner_name) == dev.name.as_deref() {
+                            debug!("Selected libpulse device found: {:?}", dev);
 
-                        self.handler.move_app_by_name(app_idx, inner_name).unwrap();
-                        break;
+                            self.handler.move_app_by_name(app_idx, inner_name).unwrap();
+                            break;
+                        }
                     }
                 }
+            }
+            Err(error) => {
+                error!("Could not list PulseAudio devices: {:?}", error);
             }
         }
 
