064d59ba4a2daa7c15f35c25926e0ea84cfeafb4
Marin <marin-m@users.noreply.github.com>
2026-03-25 06:40:32 +0100

    Use a monochrome tray icon (issue #255)

--- src/plugins/ksni.rs
+++ src/plugins/ksni.rs
@@ -1,5 +1,6 @@
 use crate::core::thread_messages::GUIMessage;
 use gettextrs::gettext;
+use image::GenericImageView;
 use ksni::TrayMethods;
 
 pub struct SystrayInterface {
@@ -10,8 +11,25 @@ impl ksni::Tray for SystrayInterface {
     fn id(&self) -> String {
         "songrec".into()
     }
-    fn icon_name(&self) -> String {
-        "re.fossplant.songrec".into()
+    /* fn icon_name(&self) -> String {
+        "re.fossplant.songrec-symbolic".into()
+    } */
+    fn icon_pixmap(&self) -> Vec<ksni::Icon> {
+        let img = image::load_from_memory_with_format(
+            include_bytes!("../../packaging/rootfs/usr/share/icons/hicolor/scalable/apps/re.fossplant.songrec-symbolic.png"),
+            image::ImageFormat::Png,
+        ).unwrap();
+        let (width, height) = img.dimensions();
+        let mut data = img.into_rgba8().into_vec();
+        assert_eq!(data.len() % 4, 0);
+        for pixel in data.chunks_exact_mut(4) {
+            pixel.rotate_right(1) // rgba to argb
+        }
+        vec![ksni::Icon {
+            width: width as i32,
+            height: height as i32,
+            data,
+        }]
     }
     fn activate(&mut self, _x: i32, _y: i32) {
         self.gui_tx.try_send(GUIMessage::ShowWindow).unwrap();
