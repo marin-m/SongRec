b6facd4a9c84efc5f5b7ebff9390445434df85f1
Marin <marin-m@users.noreply.github.com>
2026-04-09 21:26:22 +0200

    Update source code locations (cf. pull request #260)

--- translations/update_po_files.sh
+++ translations/update_po_files.sh
@@ -18,7 +18,7 @@ mv src/gui/interface.ui.h translations/
 
 # Regenerate the base ".pot" (translation template) file
 
-xgettext -kgettext -kN_ --c++ --from-code utf-8  -o translations/songrec.pot src/*.rs src/core/audio_controllers/*.rs src/core/*.rs src/core/fingerprinting/*.rs src/gui/*.rs src/plugins/*.rs src/utils/*.rs translations/interface.ui.h
+xgettext -kgettext -kN_ --c++ --from-code utf-8  -o translations/songrec.pot src/*.rs src/core/audio_controllers/*.rs src/core/*.rs src/core/fingerprinting/*.rs src/gui/*.rs src/gui/*/*.rs src/plugins/*.rs src/utils/*.rs translations/interface.ui.h
 
 for locale in translations/locale/*; do
     msgmerge --no-fuzzy-matching --update ${locale}/LC_MESSAGES/songrec.po translations/songrec.pot
